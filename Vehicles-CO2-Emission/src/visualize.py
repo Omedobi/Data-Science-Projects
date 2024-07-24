d500e270bf4ac1afd7201f81a4ff1ca6de7b7510
Anyawuike Ikenna Omedobi <100056864+Omedobi@users.noreply.github.com>
2024-07-24 21:33:36 +0100

    Add files via upload

--- Vehicles-CO2-Emission/src/visualize.py
+++ Vehicles-CO2-Emission/src/visualize.py
@@ -21,9 +21,11 @@ class EDA:
                         x=correlation_matrix.columns,
                         y=correlation_matrix.columns,
                         title="Correlation Heatmap",
-                        color_continuous_scale='RdBu_r',
+                        color_continuous_scale='Viridis',
                         zmin=-1, zmax=1,
-                        width=1320, height=840)
+                        width=1200, 
+                        height=700
+                        )
         st.plotly_chart(fig)
 
     def hist_plot(self, column: str, bins=30):
@@ -47,22 +49,22 @@ class EDA:
     def bar_plot_average_hp_by_manufacturer(self):
         """Bar plot of Average Horsepower by Manufacturer."""
         
-        average_hp_by_manufacturer = self.data.groupby('Manufacturer')['Horsepower_(HP)'].mean().reset_index()
-        fig = px.bar(average_hp_by_manufacturer, x='Manufacturer', y='Horsepower_(HP)', title='Average Horsepower by Manufacturer')
+        average_hp_by_manufacturer = self.data.groupby('Manufacturer')['Horsepower(HP)'].mean().reset_index()
+        fig = px.bar(average_hp_by_manufacturer, x='Manufacturer', y='Horsepower(HP)', title='Average Horsepower by Manufacturer')
         st.plotly_chart(fig)
 
     def pie_chart_vehicle_type_distribution(self):
         """Pie chart of Vehicle Type Distribution."""
         
-        vehicle_type_distribution = self.data['Vehicle_Type'].value_counts().reset_index()
-        vehicle_type_distribution.columns = ['Vehicle_Type', 'Count']
-        fig = px.pie(vehicle_type_distribution, values='Count', names='Vehicle_Type', title='Vehicle Type Distribution', hole=0.3)
+        vehicle_type_distribution = self.data['VehicleType'].value_counts().reset_index()
+        vehicle_type_distribution.columns = ['VehicleType', 'Count']
+        fig = px.pie(vehicle_type_distribution, values='Count', names='VehicleType', title='Vehicle Type Distribution', hole=0.3)
         st.plotly_chart(fig)
 
     def stacked_bar_drivetrain_by_manufacturer(self):
         """Stacked bar chart of Drivetrain Distribution by Manufacturer."""
         
-        drivetrain_columns = ['Drivetrain_Front', 'Drivetrain_4WD', 'Drivetrain_Rear']
+        drivetrain_columns = ['DrivetrainFront', 'Drivetrain4WD', 'DrivetrainRear']
         drivetrain_distribution = self.data.groupby('Manufacturer')[drivetrain_columns].sum().reset_index()
         fig = go.Figure()
         for col in drivetrain_columns:
@@ -77,17 +79,17 @@ class EDA:
     def violin_plot_mpg_by_regulatory_class(self):
         """Violin plot of MPG by Regulatory Class."""
         
-        fig = px.violin(self.data, x="Regulatory_Class", y="Mean_Real_World_MPG", box=True, title='Violin Plot of MPG by Regulatory Class')
+        fig = px.violin(self.data, x="RegulatoryClass", y="AvgRealWorld_MPG", box=True, title='Violin Plot of MPG by Regulatory Class')
         st.plotly_chart(fig)
 
     def bar_plot_co2_by_transmission_type(self):
         """Bar plot of Average CO2 Emissions by Transmission Type."""
         
-        transmission_columns = ['Transmission_Manual', 'Transmission_Automatic', 'Transmission_Lockup', 'Transmission_CVT_(Hybrid)', 'Transmission_Other']
-        data_melted = self.data.melt(id_vars=['Mean_CO2_(g/mi)'], value_vars=transmission_columns, var_name='Transmission_Type', value_name='Presence')
+        transmission_columns = ['TransmissionManual', 'TransmissionAutomatic', 'TransmissionLockup', 'TransmissionCVT_(Hybrid)', 'Transmission_Other']
+        data_melted = self.data.melt(id_vars=['AvgCO2(g/mi)'], value_vars=transmission_columns, var_name='Transmission_Type', value_name='Presence')
         data_melted = data_melted[data_melted['Presence'] == 1]
-        average_co2_by_transmission = data_melted.groupby('Transmission_Type')['Mean_CO2_(g/mi)'].mean().reset_index()
-        fig = px.bar(average_co2_by_transmission, x='Transmission_Type', y='Mean_CO2_(g/mi)', title='Average CO2 Emissions by Transmission Type')
+        average_co2_by_transmission = data_melted.groupby('Transmission_Type')['AvgCO2(g/mi)'].mean().reset_index()
+        fig = px.bar(average_co2_by_transmission, x='Transmission_Type', y='AvgCO2(g/mi)', title='Average CO2 Emissions by Transmission Type')
         st.plotly_chart(fig)
 
     def show_plots(self):
@@ -95,11 +97,11 @@ class EDA:
         self.desc_statistics()
         self.correlation_heatmap()
 
-        if 'Mean_CO2_(g/mi)' in self.data.columns:  
-            self.hist_plot('Mean_CO2_(g/mi)')
-            self.box_plot('Mean_CO2_(g/mi)')
-        if 'Horsepower_(HP)' in self.data.columns and 'Weight_(lbs)' in self.data.columns: 
-            self.scatter_plot('Horsepower_(HP)', 'Weight_(lbs)')
+        if 'AvgCO2(g/mi)' in self.data.columns:  
+            self.hist_plot('AvgCO2(g/mi)')
+            self.box_plot('AvgCO2(g/mi)')
+        if 'Horsepower(HP)' in self.data.columns and 'Weight(lbs)' in self.data.columns: 
+            self.scatter_plot('Horsepower(HP)', 'Weight(lbs)')
 
         self.bar_plot_average_hp_by_manufacturer()
         self.pie_chart_vehicle_type_distribution()
